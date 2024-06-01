b8dfae97026359e91d4fc88ed3a52f494ade9c88
buckapis <buckapis@gmail.com>
2024-06-01 14:45:07 -0500

    useful-swiper removed

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,7 +3,7 @@ import { GlobalService } from '../../services/global.service';
 import { CommonModule } from '@angular/common';
 import { virtualRouter } from '@app/services/virtualRouter.service';
 import { AuthRESTService } from '@app/services/auth-rest.service';
-import { NgxUsefulSwiperModule } from 'ngx-useful-swiper';
+// import { NgxUsefulSwiperModule } from 'ngx-useful-swiper';
 import Swiper from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
